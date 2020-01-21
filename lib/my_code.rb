6dd96b2d1e5207a1855e83c196f4c954acd5c35a
Jack T <jackmt9@gmail.com>
2020-01-21 20:14:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,31 +1,5 @@
-#what is a block and what does it look like?
-
-# do => end 
-
-
-# [1,2,3].map do |num|
-#   num + 1
-# end 
-
- [1,2,3].map {|num| num + 1}
-
-
-#yield accepts an argument 
-
-def add_numbers(num1, num2)
-  total = num1 +num2
-  yield(total)
-  total
-end 
-
-add_numbers(1, 2) do |t|
-  puts "The total is #{t}"
-end 
-
-add_numbers(2, 2) do |x|
-  puts "That adds up to #{x}"
-end 
-
-# map vs reduce #enumerables
-
-#map 
+def map(array)
+  i = 0 
+  new_array = []
+  while array.length > i do
+    new_array.push(yield)
